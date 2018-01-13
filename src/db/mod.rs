6c608daa439765a40d619f8e09e9bbc37578e4be
David Yip <yipdw@member.fsf.org>
2018-01-13 13:52:34 -0600

    Apply rustfmt to src/db/mod.rs
    rustfmt was applied to master; doing it here (with the same
    rustfmt.toml) will hopefully avoid the nastier merge conflicts.

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -9,7 +9,7 @@ use diesel::sqlite::SqliteConnection;
 use diesel::pg::PgConnection;
 
 use rocket::request::{self, FromRequest};
-use rocket::{Request, State, Outcome};
+use rocket::{Outcome, Request, State};
 use rocket::http::Status;
 
 pub mod schema;
@@ -24,44 +24,41 @@ type DieselConnection = SqliteConnection;
 #[cfg(all(not(feature = "sqlite"), feature = "postgres"))]
 type DieselConnection = PgConnection;
 
-
 /// Convenient type alias for the postgres database pool so we don't have to type this out.
 type Pool = r2d2::Pool<ConnectionManager<DieselConnection>>;
-
 
 /// Type alias for the pooled connection.
 type PooledConnection = r2d2::PooledConnection<ConnectionManager<DieselConnection>>;
 
-
 /// Initializes a new connection pool for the database at `url`.
 pub fn init_connection_pool<S>(url: S) -> Result<Pool, r2d2::Error>
-    where S: Into<String>
+where
+    S: Into<String>,
 {
     let manager = ConnectionManager::<DieselConnection>::new(url);
 
-    r2d2::Pool::builder()
-        .build(manager)
+    r2d2::Pool::builder().build(manager)
 }
 
 /// Request guard type for handing out db connections from the pool.
 pub struct Connection(pub PooledConnection);
-
 
 /// Custom guard implementation so routes can grab a database connection easily.
 ///
 /// Attempts to retrieve a single connection from the database pool;
 /// if no pool is online, fails with `InternalServerError`.
 /// If no connections are available, fails with `ServiceUnavailable`.
-impl <'a, 'r> FromRequest<'a, 'r> for Connection {
+impl<'a, 'r> FromRequest<'a, 'r> for Connection {
     type Error = ();
 
     fn from_request(request: &'a Request<'r>) -> request::Outcome<Connection, ()> {
         // retrieve the database connection from Rocket's managed data
         let pool = request.guard::<State<Pool>>()?;
 
-        match pool.get() { // .get() a connection from the pool
+        match pool.get() {
+            // .get() a connection from the pool
             Ok(conn) => Outcome::Success(Connection(conn)),
-            Err(_) => Outcome::Failure((Status::ServiceUnavailable, ()))
+            Err(_) => Outcome::Failure((Status::ServiceUnavailable, ())),
         }
     }
 }
